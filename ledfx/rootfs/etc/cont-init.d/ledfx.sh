70cddd3e9d310590bd1eb4bccfe2ecce6e6a41aa
Paul Sinclair <sinclairpaul@hotmail.com>
2020-02-29 12:40:26 -0500

    🔨Generate config to bind to localhost

--- ledfx/rootfs/etc/cont-init.d/ledfx.sh
+++ ledfx/rootfs/etc/cont-init.d/ledfx.sh
@@ -8,3 +8,16 @@ if ! bashio::fs.directory_exists "/data/ledfx"; then
     bashio::log "Creating config directory"
     mkdir -p /data/ledfx
 fi
+
+#Generate config if not exist
+if ! bashio::fs.file_exists "/data/ledfx/config.yaml"; then
+    bashio::log "Creating default config"
+    {
+            echo "dev_mode: false"
+            echo "devices: []"
+            echo "host: 127.0.0.1"
+            echo "max_workers: 10"
+            echo "port: 8888"
+            echo "presets: {}"
+    } >> "/data/ledfx/config.yaml"
+fi
